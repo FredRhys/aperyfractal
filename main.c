e55cf01c9b6bbfca5c8f55dc7e41eac521fe075c
Ahmburt <fredrhys@gmail.com>
2025-09-13 17:23:10 +0100

    Made initial mainloop function, corrected mainloop initialisation test

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <mpfr.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 // the rounding type used by this program; used when initializing MPFR variables
 #define R MPFR_RNDN
 // used as null pointer by the program; used to terminate lists of MPFR variables in variadic functions
@@ -11,7 +12,12 @@ int str_to_int(char *str) {
   return (int) strtol(str, 0, 10);
 }
 
-int main (int argnum, char **args) {
+int mainloop(int width, int prec) {
+  return 0;
+}
+
+int main(int argnum, char **args) {
+  // quit program if incorrect number of args given
   if (argnum != 3) {
     return -1;
   }
@@ -20,7 +26,12 @@ int main (int argnum, char **args) {
 
   // the precision used during MPFR's calculations
   const int prec = str_to_int(args[2]);
-  
+
+  // if both `width` and `prec` are valid ints, run the mainloop
+  if (width > 0 && prec > 0) {
+    mainloop(width, prec);
+  }
+
   printf("%d, %d\n", width, prec);
   return 0;
 }
